92d3170efa22a41bf0b4f85d3b6a76ed7cee2d34
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2024-05-29 21:22:38 +0200

    Fix bug in category model

--- models/category.js
+++ models/category.js
@@ -3,11 +3,10 @@ const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
     name: { type: String, required: true},
-    description: { type: String, required: true},
     uri: { type: String, required: true}
 })
 
-PlantSchema.virtual("url").get(function () {
+CategorySchema.virtual("url").get(function () {
     return  `/${this.uri}`
 })
 
