97e94dc9bce3681408ee9fb3802df516e6d07829
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2024-06-02 19:10:23 +0200

    Create route and view for plant url

--- controllers/plantController.js
+++ controllers/plantController.js
@@ -1,6 +1,19 @@
 const Plant = require("../models/plant")
+const Category = require("../models/category")
 const asyncHandler = require("express-async-handler")
 
 exports.plant_detail = asyncHandler( async (req, res, next) => {
-    res.send('Here comes the plant detail')
+
+    const [searchedCategory, searchedPlant] = await Promise.all([
+        Category.findOne({ uri: req.params.catUri}).exec(),
+        Plant.findOne({ uri: req.params.plantUri}).exec()
+    ])
+    
+    if (searchedCategory === null || searchedPlant === null) {
+        const err = new Error("Page not found")
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("plant", { plant: searchedPlant})
 })
