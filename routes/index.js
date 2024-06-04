755bac64ed95087f99566c67b3fc85ebe1a0d556
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2024-06-04 22:48:38 +0200

    Create basic view for add category form view

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Verde Vivarium' });
 });
 
+router.get('/add-category', function(req, res, next) {
+  res.render('add_category_form', { title: 'Add category' });
+})
+
 router.get("/:catUri", category_controller.category_detail);
 
 router.get("/:catUri/:plantUri", plant_controller.plant_detail);
